cb8ab51302045e43015b7a0685f93ee54e4b57a4
kuramasa <kuramasa.green@gmail.com>
2021-08-23 21:44:08 +0900

    add delete request

--- src/main/java/com/dive/divewebapi/controller/UserController.java
+++ src/main/java/com/dive/divewebapi/controller/UserController.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -106,6 +107,29 @@ public class UserController {
       TUser updatedUser = userservice.update(updateUserEntity);
 
       return ResponseEntity.ok(updatedUser);
+
+    } catch (UserNotFoundException e) {
+
+      e.setMessage("This user not found.");
+      System.err.println(e.getMessage());
+
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  ResponseEntity<TUser> deleteUser(@PathVariable String id) {
+
+    Integer userId = Integer.parseInt(id);
+
+    try {
+      Optional<TUser> userEntity = userservice.getById(userId);
+
+      TUser deleteUserEntity = userEntity.get();
+
+      TUser deletedUserEntity = userservice.delete(deleteUserEntity);
+
+      return ResponseEntity.ok(deletedUserEntity);
 
     } catch (UserNotFoundException e) {
 
